4aa9d7ac60065fc300db509a7f4ff2ef64e428e6
Christine Luu <cluu908@gmail.com>
2014-05-23 01:48:42 -0700

    most recent shit

--- beatsheets.R
+++ beatsheets.R
@@ -1,36 +1,41 @@
-movies <- c("Parsed_awalktoremember_g2.txt", "Parsed_Bruce-Almighty_g2.txt", "Parsed_Collateral-Damage_g2.txt",
-            "Parsed_Final-Destination-2_g2.txt", "Parsed_Gladiator_g2.txt", "Parsed_Grudge,-The_g2.txt", "Parsed_Harold-and-Kumar-Go-to-White-Castle_g2.txt",
-            "Parsed_hotel_rwanda_g2.txt", "Parsed_insomnia_g2.txt", "Parsed_I,-Robot_g2.txt", "Parsed_Land-of-the-Dead_g2.txt",
-            "Parsed_Legally-Blonde_g2.txt", "Parsed_Manchurian-Candidate,-The_g2.txt", "Parsed_Titanic_g2.txt")
-scenes <- c(224, 137, 188, 100, 86, 116, 138, 197, 165, 163, 152, 127, 150, 195)
-numMovies <- seq(1,14)
-numDims <- seq(1,3)
+library(fda)
+filepath <- "~/Research/Holdout_set/"
+movies <- list.dirs(path = filepath, full.names = FALSE, recursive = FALSE)
+#scenes <- c(224, 137, 187, 100, 86, 116, 137, 197, 165, 163, 152, 126, 150, 195)
+numMovies <- seq(1,length(movies))
+#numDims <- seq(1,3)
+numDims <- 1
 
-#movies <- c("Parsed_awalktoremember_g2.txt")
+#movies <- c("Parsed_Final-Destination-2_g2.txt")
 #scenes <- c(224)
 #numMovies <- 1
 
-for (dim in numDims) {
-  for (m in numMovies) {
+resvar_table = c()
+resvar_ratio = c()
+
+for (m in numMovies) {
+  for (dim in numDims) {
     
-    xtabloc <- paste("~/Research/Scripts/", movies[m], "/xtab.txt", sep="");
-    allwordsloc <- paste("~/Research/Scripts/", movies[m], "/allwords.txt", sep="");
+    xtabloc <- paste(filepath, movies[m], "/xtab.txt", sep="");
+    allwordsloc <- paste(filepath, movies[m], "/allwords.txt", sep="");
     caloc <- ("~/Research/ca.r");
+    fdaloc <- ("~/Research/fdafeatures.R");
     clickloc <- ("~/Research/clink_hcluswtd.r");
     tt <- movies[m] #("Titanic");
     
     xtab <- read.table(xtabloc);
     allwords <- read.table(allwordsloc);
     source(caloc);
+    source(fdaloc);
     xtabx <- ca(t(xtab));
     source(clickloc);
     
     wordcount = apply(xtab,2,sum);
-    totalwords = sum(wordcount);
-    cutoffpercent = 0.0009;
+    totalwords = sum(wordcount); # total words in the entire script
+    cutoffpercent = 0.00099; #0.00099
     wordcutoff = min(which(wordcount <= totalwords*cutoffpercent))
     wordsel <- c(1:wordcutoff); 
-    wordsel2 <- which(xtabx$rcntr[1:wordcutoff,1:3] < mean(xtabx$rcntr[1:wordcutoff,1:3]) + 3*sd(xtabx$rcntr[1:wordcutoff,1:3]))
+    wordsel2 <- which(xtabx$rcntr[1:wordcutoff,1] < mean(xtabx$rcntr[1:wordcutoff,1]) + 3*sd(xtabx$rcntr[1:wordcutoff,1]))
     ytab <- xtab[,wordsel2];
     
     #cutoffselection <- c(1:1000);
@@ -40,21 +45,141 @@ for (dim in numDims) {
     apply(ytab,1,sum);
     source(caloc);
     ytaby <- ca(t(ytab));
-    numscenes <- scenes[m];
+    numscenes <- dim(xtab)[1];
     
-    x1 <- ytaby$cproj[1:numscenes,dim]
-    png(paste('~/Research/RPlots/Beat Sheets/Dimension ',dim,'/',movies[m],'.png',sep=''))
-    plot(x1,type="l")
-    #x1f <- filter(x1, rep(1, 3), sides = 2)
-    x1f <- filter(x1, c(0.5, 1, 0.5), sides = 2)
+    # Beat sheet graph
+    x1 <- ytaby$cproj[1:numscenes,dim]  
+    #     png(paste('~/Research/RPlots/Beat Sheets/Dimension ',dim,'/',movies[m],'.png',sep=''))
+    #     plot(x1,type="l")
+    #     #x1f <- filter(x1, rep(1, 3), sides = 2)
+    #     x1f <- filter(x1, c(0.5, 1, 0.5), sides = 2)
     x1f <- filter(x1, c(0.25, 0.5, 0.75, 1, 0.75, 0.5, 0.25), sides = 2)
-    plot(x1f,type="l")
-    title(tt)
-    #bs2name <- c("Theme Stated","Catalyst","Break into 2","B Story","Midpoint","All Is Lost","Break into 3")
-    bs2sect <- c("T","C","R2","B","M","L","R3")
+    #     plot(x1f,type="l")
+    #     title(tt)
+    #     #bs2name <- c("Theme Stated","Catalyst","Break into 2","B Story","Midpoint","All Is Lost","Break into 3")
+    #bs2sect <- c("T","C","R2","B","M","L","R3")
+    bs2sect <- c("T","C","R2","M","L","R3")
     bs2page <- c(5,12,25,30,55,75,85)
-    text(bs2page*numscenes/110,x1f[bs2page*numscenes/110],bs2sect,col="red")
-    grid()
-    dev.off()
+    #     text(bs2page*numscenes/110,x1f[bs2page*numscenes/110],bs2sect,col="red")
+    #     grid()
+    #     dev.off()
+    
+    # Generate features
   }
+  # fda features
+  file6feats <- "~/Research/Data/6feats.csv"
+  file22feats <- "~/Research/Data/22feats.csv"
+  #fdafeatures(x1, x1f, file7feats, file22feats, resval_table, resval_ratio, numscenes)
+  #DO THIS PART:
+  # --------------
+  # setting the time index (scene numbers more precisely)
+  #library('fda')
+  #full x1
+  fdatime = (1:length(x1))
+  numsplines = 44
+  #smoothed version of x1
+  #fdatime = (1:length(x1f))
+  
+  #creating the basis functions
+  basis.range = range(fdatime);
+  bsbasis = create.bspline.basis(basis.range,numsplines);
+  #plot(bsbasis[1:5])
+  Xmat = eval.basis(fdatime,bsbasis);
+  
+  #SKIP THIS PART - WE ARE GOING TO USE ALL 22
+  #selecting a subset of basis functions to fit the beat-sheet predicted locations
+  # bs2basis <- c(5,12,25,30,55,75,85)/110*22
+  # Xsel = Xmat[,bs2basis]
+  # a = lm(x1f~Xsel)
+  
+  # the program creates a new index ii to replace the old bs2page
+  # it uses the wordcount from xtabx
+  scenewords = apply(xtab,1,sum)
+  cumwords = cumsum(scenewords)
+  totalwords = sum(scenewords)
+  k = totalwords/110;
+  pagenum = cumwords/k
+  
+  ii = NULL # scene number 
+  for (i in seq(bs2page)){
+    ii[i]=max(which(pagenum<bs2page[i]))
+    if(is.infinite(ii[i])) {ii[i] = 1}
+    ii[i] = ii[i]+1 # fix the off by one error
+  }
+  
+  bs2sect <- c("T","C","R2","B","M","L","R3")
+  bs2page <- c(5,12,25,30,55,75,85)
+#   png(paste('~/Research/RPlots/Beat Sheets/Adjusted/',movies[m],'_dim',dim,'.png',sep=''))
+   plot(x1f,type='l')
+#   #new page order
+   text(ii,x1f[ii],bs2sect)
+#   #compare to the old the old method
+   text(bs2page*numscenes/110,x1f[bs2page*numscenes/110],bs2sect,col="red")
+#   title(tt)
+#   dev.off()
+  
+  # SELECT THE BEAT SHEET FEATURES
+  #now to use it with the fda package, the old bs2basis should be replaced with the new ii
+   bs2basis <- round(ii/numscenes*numsplines)
+   # TODO might be off by one since first number is intercept
+   Xsel = Xmat[,bs2basis]
+   a = lm(x1f~Xsel)
+  
+  #CONTINUE FROM HERE
+  #least square fit of x1 (or x1f) to the basis functions
+  a2 = lm(x1~Xmat) 
+#   a = lm(x1f~Xmat)
+  
+  #IT WOULD HELP PLOTTING THESE GRAPHS FOR ALL THE FILMS FOR LATER REVIEW 
+  png(paste('~/Research/RPlots/Basis/',movies[m],'.png',sep=''))
+#   plot(x1f,type='l',col='red') #division by 4 due to the filter coefficients that sums to 4
+  plot(x1,type='l',col='red')
+  title(tt)
+  lines(a2$fitted)
+#   lines(a$fitted)
+  dev.off()
+  
+  #THESE ARE THE FEATURES TO BE PLUGGED INTO DECISION TREE
+  #the features should be the coefficients. do this for x1, x2, and possibly combined x1 & x2
+  #a$coefficients
+  #a2$coefficients
+  #cat(a$coefficients,file=file6feats,sep=',',append=TRUE)
+  #cat("\n",file=file6feats,sep='',append=TRUE)
+  cat(a2$coefficients,file=file22feats,sep='\t',append=TRUE)
+  cat("\n",file=file22feats,sep='',append=TRUE)
+  
+  #KEEP A TABLE WITH THESE 2 NUMBERS FOR LATER REVIEW AS WELL
+  #in addition to using the coefficients in prediction tree pring out resvar and the ratio
+  #error
+  resvar = var(a2$residuals)
+  resvar_table = c(resvar_table, resvar)
+  #ratio of residual variance and the x1 variance
+  resratio = resvar/var(x1)
+  resvar_ratio = c(resvar_ratio, resratio)
+
+  # want the dist between all scenes
+  #   png(paste('~/Research/RPlots/Beat Sheets/scene_dist/',movies[m],'_dim2.png',sep=''))
+  #   s1 <- ytaby$cproj[1:numscenes-1,1:2]
+  #   s2 <- ytaby$cproj[2:numscenes,1:2]
+  #   euclDist <- sqrt(rowSums((s1-s2)^2))
+  #   plot(euclDist, type="l")
+  #   title(paste(movies[m],"2 Dimensions",sep='-'))
+  #   bs2sect <- c("T","C","R2","B","M","L","R3")
+  #   bs2page <- c(5,12,25,30,55,75,85)
+  #   text(bs2page*numscenes/110,euclDist[bs2page*numscenes/110],bs2sect,col="red")
+  #   grid()
+  #   dev.off()
+  #   
+  #   png(paste('~/Research/RPlots/Beat Sheets/scene_dist/',movies[m],'_dim3.png',sep=''))
+  #   s1 <- ytaby$cproj[1:numscenes-1,1:3]
+  #   s2 <- ytaby$cproj[2:numscenes,1:3]
+  #   euclDist <- sqrt(rowSums((s1-s2)^2))
+  #   plot(euclDist, type="l")
+  #   title(paste(movies[m],"3 Dimensions",sep='-'))
+  #   text(bs2page*numscenes/110,euclDist[bs2page*numscenes/110],bs2sect,col="red")
+  #   grid()
+  #   dev.off()
+  
 }
+cat(resvar_table,file="~/Research/Data/resvar.tsv",sep='\n')
+cat(resvar_ratio,file="~/Research/Data/resrat.tsv",sep='\n')
--- trees.R
+++ trees.R
@@ -1,16 +1,30 @@
 library(rpart)
 # dataIn <- read.table("~/Research/Data/Movie Results (Cleaned) - New Movies.csv", header=TRUE, sep=",")
-dataIn <- read.csv("~/Research/Data/Movie Results (Cleaned) - New Movies2.csv")
+dataIn <- read.csv("~/Research/Data/Movie Results (Cleaned) - Final Training Set - 44 - 1st dim 2.csv")
 dataIn = dataIn[dataIn[,1]!="Juno",] # Remove Juno
-dataIn = dataIn[dataIn[,3]=="Comedy",]
-dataIn <- dataIn[,6:dim(dataIn)[2]] # Get rid of first col with movie names
+#dataIn = dataIn[dataIn[,3]=="Comedy",]
+dataIn <- dataIn[,8:dim(dataIn)[2]] # Get rid of first col with movie names
 #dataIn <- dataIn[,4:31] # with genres
+
+# pc <- dataIn[,-1]
+# pc_res <- princomp(pc)
+# topvecs <- as.matrix(pc_res$loadings[,1:22])
+# res <- as.matrix(pc) %*% topvecs
+# res <- data.frame(dataIn[,1],res)
+# names(res)[1]="ROI"
+# dataIn = res
+
+# Get genres
+genreData <- read.csv("~/Research/Data/Movie Results (Cleaned) - Final Train Genres.csv")
+genreData = genreData[genreData[,1]!="Juno",] # Remove Juno
+genreData <- genreData[,-1] # Take out movie names
+dataIn <- data.frame(dataIn,genreData)
 # 
 nTrees <- c(30, 40, 50, 60, 70, 80, 100)
-minSplit <- c(2, 3, 4, 5)
+minSplit <- c(4, 5, 6, 7, 8)
 cpVals <- c(0.1, 0.01, 0.001, 0.0001)
-# nTrees <- c(100)
-# minSplit <- c(2)
+# nTrees <- c(50)
+# minSplit <- c(8)
 # cpVals <- c(0.0001)
 R2scores <- rep(0, length(nTrees)*length(minSplit)*length(cpVals))
 R2index = 1
@@ -24,7 +38,7 @@ c_final = 0
 for (numTrees in nTrees) {
   for (m in minSplit) {
     for (c in cpVals) {
-      set.seed(692)
+      set.seed(100)
       numExamples <- nrow(dataIn)
       treeIndices <- seq(1, numTrees)
       treelist <- vector(mode="list", length=numTrees)
@@ -33,9 +47,14 @@ for (numTrees in nTrees) {
       for (tree in treeIndices) {
         sampleInd = sample.int(numExamples, size = 0.8*numExamples, replace = TRUE)
         subsample = dataIn[sampleInd,]
-        # train the tree
-        treelist[[tree]] <- rpart(ROI~., data = subsample, method = "anova", control=rpart.control(minsplit=m, cp=c))
+        
+        #allInd = seq(1,numExamples)
+        #testgroup = allInd[-sampleInd]
+        #testInd = sample(testgroup, size = 0.2*numExamples, replace = FALSE)
+        
+        treelist[[tree]] <- rpart(ROI~., data = subsample, method = "anova", control=rpart.control(minsplit=2, cp=c, maxdepth=m))
         # prediction for this particular tree
+        #predlist[[tree]] <- predict(treelist[[tree]], newdata = dataIn[testInd,], type = "vector")
         predlist[[tree]] <- predict(treelist[[tree]], newdata = dataIn, type = "vector")
       }
       
@@ -52,16 +71,16 @@ for (numTrees in nTrees) {
       #printcp(treelist[[1]]) # display the results
       #summary(treelist[[1]]) # detailed summary of splits
       
-      MSE = sum((finalPrediction-dataIn[,1])^2)/numExamples
-      MAE = sum(abs(finalPrediction-dataIn[,1]))/numExamples
       E_data = mean(dataIn[,1])
+      MSE = sum((finalPrediction-E_data)^2)/numExamples
+      MAE = sum(abs(finalPrediction-E_data))/numExamples
       # how well it will fit to future data -- closer to 1 is better
       R2 = 1 - sum((finalPrediction-dataIn[,1])^2)/sum((dataIn[,1]-E_data)^2)
       R2scores[R2index] = R2
       R2index = R2index + 1
       
-      #if (MSE < minMSE) {
-      if (R2 > maxR2) {
+      if (MSE < minMSE) {
+      #if (R2 > maxR2) {
         t_final = numTrees
         m_final = m
         c_final = c
@@ -83,6 +102,49 @@ for (numTrees in nTrees) {
 t_final
 m_final
 c_final
+
+
+#########
+# Holdout set
+holdoutSet <- read.csv("~/Research/Data/Movie Results (Cleaned) - Holdout Set - 44 - 2.csv")
+#dataIn = dataIn[dataIn[,3]=="Comedy",] # choose specific genre
+holdoutSet <- holdoutSet[,9:dim(holdoutSet)[2]-1] # Get rid of first col with movie names
+# 
+# hpc <- holdoutSet[,-1]
+# res <- as.matrix(hpc) %*% topvecs
+# res <- data.frame(holdoutSet[,1],res)
+# holdoutSet = res
+
+hgenreData <- read.csv("~/Research/Data/Movie Results (Cleaned) - Holdout Set Genres.csv")
+hgenreData <- hgenreData[,-1] # Take out movie names
+holdoutSet <- data.frame(holdoutSet,hgenreData)
+
+hpredlist <- vector(mode="list", length=numTrees)
+# For bag-CART, sample with replacement and create trees
+for (tree in treeIndices) {
+  # prediction for this particular tree
+  hpredlist[[tree]] <- predict(treelist[[tree]], newdata = holdoutSet, type = "vector")
+}
+
+# do an element-wise addition of all the predictions
+numHoldout = nrow(holdoutSet)
+hsums = rep(0, numHoldout)
+for (tree in treeIndices) {
+  hsums = hsums + hpredlist[[tree]]
+}
+# Final predcted movie gross for bag-CART is the average prediction from
+# all the trees in the ensemble.
+hfinalPrediction = hsums / numTrees
+
+# Random toy code for viewing trees
+#printcp(treelist[[1]]) # display the results
+#summary(treelist[[1]]) # detailed summary of splits
+
+hE_data = mean(holdoutSet[,1])
+hMSE = sum((hfinalPrediction-hE_data)^2)/numHoldout
+hMAE = sum(abs(hfinalPrediction-hE_data))/numHoldout
+# how well it will fit to future data -- closer to 1 is better
+hR2 = 1 - sum((hfinalPrediction-holdoutSet[,1])^2)/sum((holdoutSet[,1]-hE_data)^2)
 
 #########
 #relevance index (this code is broken, get relevance index from the next one)
